d21beb79695ffbf192267b3ff20b02cac017fd62
duchoang <ducdongvong@gmail.com>
2024-12-17 22:18:53 +0700

    [Ql-1] Add ProductEditView

---
+++ QLSX.Module.Products/Models/EditProductData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSX.Module.Products.Models
+{
+    public class EditProductData
+    {
+        public BigInteger ID { get; set; }
+        public int Step { get; set; }
+        public string StepName { get; set; }
+        public DateTime ExpectDay { get; set; }
+        public DateTime ActualDay { get; set; }
+        public ProductionStatus Status { get; set; }
+
+    }
+
+    public enum ProductionStatus
+    {
+        Done,
+        Inprogress,
+        None,
+        Missing
+    }
+}
